05367466689a59846034d414f28b18286ace4182
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-01-25 14:24:57 +0100

    Create newton_verfahren.py

---
+++ newton_verfahren.py
@@ -0,0 +1,37 @@
+#!/bin/python
+
+
+class NewtonVerfahren:
+    def __init__(self, start_x, iterations):
+        self.start_x = start_x
+        self.iterations = iterations
+        self.main_loop()
+
+    def main_loop(self):
+        for i in range(self.iterations):
+            try:
+                new_x = self.return_zero(self.return_m(self.start_x), self.return_b(self.return_y(self.start_x), self.return_m(self.start_x), self.start_x))
+            except ZeroDivisionError:
+                break
+            self.start_x = new_x
+            print(self.start_x)
+
+    def return_y(self, x):
+        y = x ** 2 - 5
+        return y
+
+    def return_m(self, x):
+        f_ = 2 * x
+        return f_
+
+    def return_b(self, y, m, x):
+        b = y - m * x
+        return b
+
+    def return_zero(self, m, b):
+        x_zero = -1 * b / m
+        return x_zero
+
+
+if __name__ == '__main__':
+    test = NewtonVerfahren(10000, 50)
