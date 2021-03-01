e57d2b4b02f8b5892c1bedccf10776c19563482f
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-03-01 17:22:58 +0100

    Create word_counter.py

---
+++ word_counter.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+
+class WordCounter:
+    def __init__(self, file):
+        self.file = file
+
+    def count(self):
+        with open(self.file) as file:
+            data = file.read()
+            words = data.split(' ')
+            print(len(words))
+
+
+if __name__ == '__main__':
+    counter = WordCounter("test.txt")
+    counter.count()
