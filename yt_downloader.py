38f6d589b4dbd1c3834b43a89cabbee30ecbcaf0
Jogi123 <70063275+Jogi123@users.noreply.github.com>
2021-08-05 18:28:16 +0200

    Add files via upload

---
+++ yt_downloader.py
@@ -0,0 +1,13 @@
+import pytube
+url = str(input('Enter video url: '))
+
+youtube = pytube.YouTube(url)
+streams = youtube.streams.all()
+for stream in streams:
+    print(stream)
+
+which_stream = int(input('Enter itag for stream: '))
+video = youtube.streams.get_by_itag(which_stream)
+print('Downloading video...')
+video.download('/home/jogi/Downloads')
+print('Done')
