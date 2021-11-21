1e77e357b05ca9ffc7e32bdfeb5c215c06314e05
Jogi123 <testmail@test.com>
2021-11-21 17:06:12 +0100

    added formatted output

--- matrix_addition.py
+++ matrix_addition.py
@@ -9,16 +9,25 @@ class MatrixAddition:
             for column in range(len(self.matrix1[0])):
                 self.result[line][column] = self.matrix1[line][column] + self.matrix2[line][column]
 
-        for i in self.result:
-            print(i)
+        return self.result
+
+    def __str__(self):
+        result_formatted = ""
+        for i in range(len(self.result)):
+            if i == len(self.result) - 1:
+                result_formatted += f'{self.result[i]}'
+            else:
+                result_formatted += f'{self.result[i]}\n'
+        return result_formatted
 
 
 if __name__ == '__main__':
     m1 = [[1, 1, 3],
-          [4, -2, 6]]
+          [4, -2, 8]]
 
     m2 = [[3, 9, 4],
           [4, 2, 3]]
 
     addition = MatrixAddition(m1, m2)
     addition.add()
+    print(addition)
