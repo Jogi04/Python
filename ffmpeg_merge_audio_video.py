2c1e988e7baaebebf7fe3a61e6bf70318ce410c2
Jogi123 <testmail@test.com>
2022-01-22 13:35:54 +0100

    added parameters

--- ffmpeg_merge_audio_video.py
+++ ffmpeg_merge_audio_video.py
@@ -1,12 +1,9 @@
 import os
 
 
-def merge():
-    video_file = input('Enter the path of the video file: ')
-    audio_file = input('Enter the path of the audio file: ')
-    output_file = input('Enter the filename of the output file: ')
+def merge(video_file, audio_file, output_file):
     os.system(f'ffmpeg -i "{video_file}" -i "{audio_file}" -shortest {output_file}.mp4')
 
 
 if __name__ == '__main__':
-    merge()
+    merge('video.mp4', 'audio.mp3', 'merged.mp4')
