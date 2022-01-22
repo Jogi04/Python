7f258f2c693915fc9e85b6a39340965e79424d91
Jogi123 <testmail@test.com>
2022-01-22 12:20:49 +0100

    changed to ffmpeg

--- cut_mp3_audio.py
+++ cut_mp3_audio.py
@@ -1,13 +1,13 @@
-from pydub import AudioSegment
+import os
 
 
-def cut_audio(file_path, start_secs, end_secs, output_file_name):
-    start_time = start_secs * 1000
-    end_time = end_secs * 1000
-    audio = AudioSegment.from_mp3(file_path)
-    output = audio[start_time:end_time]
-    output.export(f'{output_file_name}', format='mp3')
+def cut_audio():
+    start_time = input('Enter the start time in seconds: ')
+    end_time = input('Enter the end time in seconds: ')
+    audio_file = input('Enter the path of the audio file: ')
+    output_file = input('Enter the filename of the output file: ')
+    os.system(f'ffmpeg -i {audio_file} -ss {start_time} -to {end_time} -c:a libmp3lame {output_file}')
 
 
 if __name__ == '__main__':
-    cut_audio('test.mp3', 0, 0, 'test-cut.mp3')
+    cut_audio()
