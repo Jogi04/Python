ebbf9aa1bb0dd5e980c5807531976ea58dfa48b0
Jogi123 <testmail@test.com>
2022-01-22 12:32:58 +0100

    added parameters

--- cut_mp3_audio.py
+++ cut_mp3_audio.py
@@ -1,13 +1,9 @@
 import os
 
 
-def cut_audio():
-    start_time = input('Enter the start time in seconds: ')
-    end_time = input('Enter the end time in seconds: ')
-    audio_file = input('Enter the path of the audio file: ')
-    output_file = input('Enter the filename of the output file: ')
-    os.system(f'ffmpeg -i {audio_file} -ss {start_time} -to {end_time} -c:a libmp3lame {output_file}')
+def cut_audio(start_time, end_time, audio_file, output_file):
+    os.system(f'ffmpeg -i "{audio_file}" -ss {start_time} -to {end_time} -c:a libmp3lame "{output_file}"')
 
 
 if __name__ == '__main__':
-    cut_audio()
+    cut_audio(10, 20, 'test.mp3', 'cut.mp3')
