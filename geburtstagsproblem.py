9b6c9bc23573d2b9b6b8a27322f6cb781381115d
Jogi123 <testmail@test.com>
2022-02-08 16:46:51 +0100

    add write to file function geburtstagsproblem.py

--- geburtstagsproblem.py
+++ geburtstagsproblem.py
@@ -1,9 +1,14 @@
-def geburtstag_problem(people):
+def geburtstag_problem(people, write_to_file=True):
     prob = 1
     for i in range(1, people + 1):
-        print(i)
         prob = prob * (366 - i) / 365
+        print(i)
         print(f'Wahrscheinlichkeit: {prob}')
+        if write_to_file is True:
+            with open('geburtstagsproblem.txt', 'a') as file:
+                file.write(str(i) + '\n')
+                file.write(f'Wahrscheinlichkeit: {prob}\n')
+                file.close()
 
 
 if __name__ == '__main__':
